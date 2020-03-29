b8ba31e52913016ec08e8dd32a96aed9c5c196ea
LeonardoPena <20186923@itla.edu.do>
2020-03-29 19:11:41 -0400

    Disenio en general

--- Js/index.js
+++ Js/index.js
@@ -17,25 +17,14 @@ function eliminar(){
 function mru(){
 
 	eliminar();
-	document.getElementById("f").innerHTML = "<input type='button' onclick='location.reload();'; value='Atras'/><br>" +
+	document.getElementById("f").innerHTML = "<input type='button'  class='botone' onclick='location.reload();'; value='Atras'/><br>" +
 
-	"<label for='Distancia'>Distancia:</label><br><input type='number' id='distancia' name='distancia'> <select id='cars'>"+
-	"<option value='volvo'>Mill</option>"+
-	"<option value='saab'>KM</option>"+
-	"<option value='mercedes'>M</option>"+
-  "</select>"+ 
-	"<br><label for='velocidad'>Velocidad:</label> <br><input type='number' id='Velocidad' name='velocidad'><select id='cars'>"+
-	"<option value='volvo'>Km/H</option>"+
-	"<option value='saab'>Mi/H</option>"+
-	"<option value='mercedes'>m/seg</option>"+
-  "</select><br>"+ 
-	"<label for='tiempo'>Tiempo:</label> <br><input type='number' id='Tiempo' name='Tiempo'><select id='cars'>"+
-	"<option value='volvo'>H</option>"+
-	"<option value='saab'>Min</option>"+
-	"<option value='mercedes'>Sec</option>"+
-	"<option value='audi'>Ms</option>"+
-  "</select> <br>"+ 
-	"<br><button onclick='verVacio(); animation();'>Calcular</button>";
+	"<label class='lable' for='Distancia'>Distancia:</label><br><input type='number' id='distancia' name='distancia'> "+
+
+	"<br><label class='lable' for='velocidad'>Velocidad:</label> <br><input type='number' id='Velocidad' name='velocidad'><br>"+
+
+	"<label class='lable' for='tiempo'>Tiempo:</label> <br><input type='number' id='Tiempo' name='Tiempo'><br>"+
+	"<br><button class='botone' onclick='verVacio(); animation();'>Calcular</button>";
 
 
 
@@ -43,119 +32,74 @@ function mru(){
 
 function mruv(){
 	eliminar();
-	document.getElementById("f").innerHTML = "<input type='button' onclick='location.reload();'; value='Atras'/><br>"+
-	"<label for='VelocidadFinal'>Velocidad Final:</label> <br><input type='number' id='vFinal' name='Tiempo'><select id='cars'>"+
-	"<option value='volvo'>Km/H</option>"+
-	"<option value='saab'>Mi/H</option>"+
-	"<option value='mercedes'>m/seg</option>"+
-  "</select><br>"+ 
-  "<label for='VelocidadInicial'>Velocidad Inicial:</label> <br><input type='number' id='vInicial' name='Tiempo'><select id='cars'>"+
-	"<option value='volvo'>Km/H</option>"+
-	"<option value='saab'>Mi/H</option>"+
-	"<option value='mercedes'>m/seg</option>"+
-  "</select><br>"+ 
-	"<label for='Distancia'>Distancia:</label><br><input type='number' id='distanciaMRUV' name='distancia'> <select id='cars'>"+
-	"<option value='volvo'>Mill</option>"+
-	"<option value='saab'>KM</option>"+
-	"<option value='mercedes'>M</option>"+
-  "</select><br>"+ 
-  "<label for='Aceleracion'>Aceleracion:</label> <br><input type='number' id='aceleracionMRUV' name='Tiempo'><select id='cars'>"+
-	"<option value='volvo'>m/s^2</option>"+
-	"<option value='saab'>Km/h^2</option>"+
-	"<option value='mercedes'>Mi/h^2</option>"+
-  "</select> <br>"+
-  "<label for='tiempo'>Tiempo:</label> <br><input type='number' id='tiempoMRUV' name='Tiempo'><select id='cars'>"+
-  "<option value='volvo'>H</option>"+
-  "<option value='saab'>Min</option>"+
-  "<option value='mercedes'>Sec</option>"+
-  "<option value='audi'>Ms</option>"+
-"</select> <br>"+
+	document.getElementById("f").innerHTML = "<input type='button' class='botone' onclick='location.reload();'; value='Atras'/><br>"+
+	"<label class='lable' for='VelocidadFinal'>Velocidad Final:</label> <br><input type='number' id='vFinal' name='Tiempo'><br>"+
+
+  "<label class='lable' for='VelocidadInicial'>Velocidad Inicial:</label> <br><input type='number' id='vInicial' name='Tiempo'><br>"+
+
+	"<label class='lable' for='Distancia'>Distancia:</label><br><input type='number' id='distanciaMRUV' name='distancia'><br> "+
+
+  "<label class='lable' for='Aceleracion'>Aceleracion:</label> <br><input type='number' id='aceleracionMRUV' name='Tiempo'><br>"+
+  "<label class='lable' for='tiempo'>Tiempo:</label> <br><input type='number' id='tiempoMRUV' name='Tiempo'><br>"+
 /////////RADIO
 " <p>Por favor escoja que dato buscara:</p>"+
 "<input type='radio' id='vFinalRad' name='gender' value='male'>"+
 "<label for='male'>Velocidad Final</label><br>"+
 
 "	<input type='radio' id='vInicialRad' name='gender' value='female'>"+
-"<label for='female'>Velocidad Inicial</label><br>"+
+"<label  for='female'>Velocidad Inicial</label><br>"+
 
 "<input type='radio' id='distanciaRad' name='gender' value='female'>"+
-"<label for='female'>Distancia</label><br>"+
+"<label  for='female'>Distancia</label><br>"+
 
 "<input type='radio' id='aceleracionRad' name='gender' value='female'>"+
-"<label for='other'>Aceleracion</label>"+
+"<label  for='other'>Aceleracion</label>"+
 
 "<input type='radio' id='tiempoRad' name='gender' value='female'>"+
-"<label for='other'>Tiempo</label>"+
-"<br><button onclick='verDatos()'>Calcular</button>";
+"<label   for='other'>Tiempo</label>"+
+"<br><button class='botone' onclick='verDatos()'>Calcular</button>";
 
 }
 
 function cl(){
 	eliminar();
-	document.getElementById("f").innerHTML = "<input type='button' onclick='location.reload();'; value='Atras'/><br>"+
-	"<label for='tiempo'>Velocidad Final:</label> <br><input type='number' id='vFinalCl' name='Tiempo'><select id='cars'>"+
-	"<option value='volvo'>Km/H</option>"+
-	"<option value='saab'>Mi/H</option>"+
-	"<option value='mercedes'>m/seg</option>"+
-  "</select><br>"+ 
-  "<label for='tiempo'>Velocidad Inicial:</label> <br><input type='number' id='vInicialCl' name='Tiempo'><select id='cars'>"+
-	"<option value='volvo'>Km/H</option>"+
-	"<option value='saab'>Mi/H</option>"+
-	"<option value='mercedes'>m/seg</option>"+
-  "</select><br>"+ 
-	"<label for='Distancia'>Altura:</label><br><input type='number' id='alturaCl' name='distancia'> <select id='cars'>"+
-	"<option value='volvo'>Mill</option>"+
-	"<option value='saab'>KM</option>"+
-	"<option value='mercedes'>M</option>"+
-  "</select> <br>"+
-  "<label for='tiempo'>Tiempo:</label> <br><input type='number' id='tiempoCl' name='Tiempo'><select id='cars'>"+
-  "<option value='volvo'>H</option>"+
-  "<option value='saab'>Min</option>"+
-  "<option value='mercedes'>Sec</option>"+
-  "<option value='audi'>Ms</option>"+
-"</select> <br>"+
+	document.getElementById("f").innerHTML = "<input type='button' class='botone' onclick='location.reload();'; value='Atras'/><br>"+
+	"<label  for='tiempo'>Velocidad Final:</label> <br><input type='number' id='vFinalCl' name='Tiempo'><br>"+
+
+  "<label  for='tiempo'>Velocidad Inicial:</label> <br><input type='number' id='vInicialCl' name='Tiempo'> <br>"+
+
+	"<label  for='Distancia'>Altura:</label><br><input type='number' id='alturaCl' name='distancia'> <br>"+
+
+  "<label  for='tiempo'>Tiempo:</label> <br><input type='number' id='tiempoCl' name='Tiempo'> <br>"+
 /////////RADIO
 " <p>Por favor escoja que dato buscara:</p>"+
 "<input type='radio' id='vFinalClRad' name='gender' value='male'>"+
-"<label for='male'>Velocidad Final</label><br>"+
+"<label  for='male'>Velocidad Final</label><br>"+
 
 "	<input type='radio' id='vInicialClRad' name='gender' value='female'>"+
-"<label for='female'>Velocidad Inicial</label><br>"+
+"<label  for='female'>Velocidad Inicial</label><br>"+
 
 "<input type='radio' id='alturaClRad' name='gender' value='female'>"+
-"<label for='female'>Altura</label><br>"+
+"<label  for='female'>Altura</label><br>"+
 
 "<input type='radio' id='tiempoClRad' name='gender' value='female'>"+
-"<label for='female'>Tiempo</label><br>"+
-"<br><button onclick='BuscarDatos()'>Calcular</button>";
+"<label  for='female'>Tiempo</label><br>"+
+"<br><button class='botone' onclick='BuscarDatos()'>Calcular</button>";
 
 
 }
 
 function tv(){
 	eliminar();
-	document.getElementById("f").innerHTML = "<input type='button' onclick='location.reload();'; value='Atras'/><br>"+
-	"<label for='tiempo'>Velocidad Final:</label> <br><input type='number' id='vFinalTv' name='Tiempo'><select id='cars'>"+
-	"<option value='volvo'>Km/H</option>"+
-	"<option value='saab'>Mi/H</option>"+
-	"<option value='mercedes'>m/seg</option>"+
-  "</select><br>"+ 
-  "<label for='tiempo'>Velocidad Inicial:</label> <br><input type='number' id='vInicialTv' name='Tiempo'><select id='cars'>"+
-	"<option value='volvo'>Km/H</option>"+
-	"<option value='saab'>Mi/H</option>"+
-	"<option value='mercedes'>m/seg</option>"+
-  "</select><br>"+ 
-	"<label for='Distancia'>Altura:</label><br><input type='number' id='alturaTv' name='distancia'> <select id='cars'>"+
-	"<option value='volvo'>Mill</option>"+
-	"<option value='saab'>KM</option>"+
-	"<option value='mercedes'>M</option>"+
-  "</select> <br>"+
-  "<label for='tiempo'>Tiempo:</label> <br><input type='number' id='tiempoTv' name='Tiempo'><select id='cars'>"+
-  "<option value='volvo'>H</option>"+
-  "<option value='saab'>Min</option>"+
-  "<option value='mercedes'>Sec</option>"+
-  "<option value='audi'>Ms</option>"+
-"</select> <br>"+
+	document.getElementById("f").innerHTML = "<input type='button' class='botone' onclick='location.reload();'; value='Atras'/><br>"+
+	"<label class='lable' for='tiempo'>Velocidad Final:</label> <br><input type='number' id='vFinalTv' name='Tiempo'><br>"+
+
+  "<label class='lable' for='tiempo'>Velocidad Inicial:</label> <br><input type='number' id='vInicialTv' name='Tiempo'><br>"+
+
+	"<label class='lable' for='Distancia'>Altura:</label><br><input type='number' id='alturaTv' name='distancia'> <br>"+
+
+  "<label class='lable' for='tiempo'>Tiempo:</label> <br><input type='number' id='tiempoTv' name='Tiempo'><br>"+
+
 /////////RADIO
 " <p>Por favor escoja que dato buscara:</p>"+
 "<input type='radio' id='vFinalTvRad' name='gender' value='male'>"+
@@ -170,7 +114,7 @@ function tv(){
 "<input type='radio' id='tiempoTvRad' name='gender' value='female'>"+
 "<label for='other'>Tiempo</label>"+
 
-"<br><button onclick='FindDatos()'>Calcular</button>";
+"<br><button class='botone' onclick='FindDatos()'>Calcular</button>";
 
 
 }
