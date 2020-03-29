817f49f3139be0d9b02cb681a94dee4152553317
LeonardoPena <20186923@itla.edu.do>
2020-03-28 23:58:02 -0400

    Mira Klk Aqui

--- Js/index.js
+++ Js/index.js
@@ -6,10 +6,26 @@ function mru(){
 	bot1.style.display="none";
 	bot2.style.display="none";
 	bot3.style.display="none";
-	document.getElementById("f").innerHTML = "<input type='button' onclick='location.reload();'; value='Atras'/><br> "+ 
-	"<label for='Distancia'>Distancia:</label><br><input type='text' id='distancia' name='distancia'>"+
-	"<br><label for='velocidad'>Velocidad:</label> <br><input type='text' id='Velocidad' name='velocidad'></br>"+
-	"<label for='tiempo'>Tiempo:</label> <br><input type='text' id='Tiempo' name='Tiempo'></br>"+
+	document.getElementById("f").innerHTML = "<input type='button' onclick='location.reload();'; value='Atras'/><br>" +
+
+	"<label for='Distancia'>Distancia:</label><br><input type='text' id='distancia' name='distancia'> <select id='cars'>"+
+	"<option value='volvo'>Volvo</option>"+
+	"<option value='saab'>Saab</option>"+
+	"<option value='mercedes'>Mercedes</option>"+
+	"<option value='audi'>Audi</option>"+
+  "</select>"+ 
+	"<br><label for='velocidad'>Velocidad:</label> <br><input type='text' id='Velocidad' name='velocidad'><select id='cars'>"+
+	"<option value='volvo'>Volvo</option>"+
+	"<option value='saab'>Saab</option>"+
+	"<option value='mercedes'>Mercedes</option>"+
+	"<option value='audi'>Audi</option>"+
+  "</select><br>"+ 
+	"<label for='tiempo'>Tiempo:</label> <br><input type='text' id='Tiempo' name='Tiempo'><select id='cars'>"+
+	"<option value='volvo'>Volvo</option>"+
+	"<option value='saab'>Saab</option>"+
+	"<option value='mercedes'>Mercedes</option>"+
+	"<option value='audi'>Audi</option>"+
+  "</select> <br>"+ 
 	"<br><button onclick='verVacio()'>Calcular</button>";
 
 }
