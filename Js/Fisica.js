d2ace3a5ee000c45991ec83209f4163c80ca01df
Liam <eliamsolis2201@gmail.com>
2020-03-28 20:38:15 -0400

    funciones de conversiones agregadas

---
+++ Js/Fisica.js
@@ -0,0 +1,101 @@
+
+//Algunas Funciones de conversion
+
+
+//Magnitudes Fisicas de Velocidad
+
+function convertirKmHoras(kmHoras){
+	var kmHoras = (kmHoras*1000)/3600 ;
+	var mSegundos = kmHoras;
+	alert(mSegundos);
+
+
+}
+
+function convertirMiHoras(MiHoras){
+
+	MiHoras = (MiHoras*1609.34)/3600;
+	var mSegundos = MiHoras;
+	alert(mSegundos);
+
+}
+
+function convertirmSegundosakmHoras(mSegundos){
+	var mSegundos = (mSegundos*0.001)/0.000277778;
+	var kmHoras = Math.round(mSegundos);
+	alert(kmHoras);
+
+
+}
+
+function convertirmSegundosaMiHoras(mSegundos){
+	var mSegundos = (mSegundos*0.000621371)/0.000277778;
+	var MiHoras = Math.round(mSegundos);
+	alert(MiHoras);
+
+
+}
+
+//Magnitudes Fisicas de Velocidad ****************************************************
+
+
+
+
+
+//Magnitudes Fisicas de tiempo
+
+function convertirminutosSegundos(minutos){
+	segundos = minutos*60;
+	alert(segundos);
+
+}
+
+function convertirsegundosMinutos(segundos){
+	minutos = segundos/60;
+	alert(minutos);
+
+}
+
+function convertirhorasSegundos(horas){
+	segundos = horas*3600;
+	alert(segundos);
+
+}
+
+function convertirsegundosHoras(segundos){
+	horas = segundos/3600;
+	alert(horas);
+} 
+
+function convertirmilisegundosSegundos(milisegundos){
+	segundos = milisegundos/1000;
+	alert(segundos);
+
+}
+
+function convertirsegundosHoras(segundos){
+	horas = segundos/3600;
+	alert(horas);
+} 
+
+
+
+//convertirKmHoras(144);
+//convertirMSegundos(40);
+//convertirMiHoras(45);
+//convertirmSegundosaMiHoras(60);
+
+//convertirminutosSegundos(2);
+//convertirhorasSegundos(4);
+//convertirsegundosMinutos(120);
+//convertirsegundosHoras(120);
+
+
+
+
+
+
+
+
+
+
