ed1045cc26ed36920967fab8b743ad7c002d20e5
Liam <eliamsolis2201@gmail.com>
2020-03-28 23:23:19 -0400

    cambios

--- Js/FormulasMRU.js
+++ Js/FormulasMRU.js
@@ -1,24 +1,49 @@
 
 function encontrarVelocidad(distancia, tiempo){
-    velocidad = distancia/tiempo;
-    velocidadx = tiempo.toString();
+    velocidad = parseFloat(distancia/tiempo);
+    velocidadx = velocidad.toString();
     velocidadx = velocidadx.substring(0,5);
     alert(velocidadx);
 
 }
 
 function encontrarTiempo(distancia, velocidad){
-    tiempo = distancia/velocidad;
+    tiempo = parseFloat(distancia/velocidad);
+
     tiempox = tiempo.toString();
     tiempox = tiempox.substring(0,5);
     alert(tiempox);
 }
 
 function encontrarDistancia(velocidad, tiempo){
-    distancia = tiempo * velocidad;
+    distancia = parseFloat(tiempo * velocidad);
     distanciax = distancia.toString();
     distanciax = distanciax.substring(0,5)
     alert(distanciax);
+
+}
+
+
+function verVacio(){
+
+    distancia = document.getElementById('distancia').value;
+    velocidad = document.getElementById('Velocidad').value;
+    tiempo = document.getElementById('Tiempo').value;
+
+    if(distancia == "" && velocidad != "" && tiempo != "" ){
+        encontrarDistancia(velocidad,tiempo);
+
+    }else if(velocidad == "" && distancia != "" && tiempo != ""){
+        encontrarVelocidad(distancia,tiempo);
+
+    }else if(tiempo == "" && velocidad != "" && distancia != ""){
+        encontrarTiempo(velocidad, distancia);
+
+
+    }else{
+
+
+    }
 
 }
 
